0515f6128e5e46c04c8529e605577789742eaebb
Gustavo Alves <gugaalves92@hotmail.com>
2022-10-06 17:52:55 -0300

    feat: Layout e Funcionalidades

--- app-web-backend/Controllers/ConsumosController.cs
+++ app-web-backend/Controllers/ConsumosController.cs
@@ -9,22 +9,23 @@ using app_web_backend.Models;
 
 namespace app_web_backend.Controllers
 {
-    public class VeiculoController : Controller
+    public class ConsumosController : Controller
     {
         private readonly ApplicationDbContext _context;
 
-        public VeiculoController(ApplicationDbContext context)
+        public ConsumosController(ApplicationDbContext context)
         {
             _context = context;
         }
 
-        // GET: Veiculo
+        // GET: Consumos
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Veiculos.ToListAsync());
+            var applicationDbContext = _context.Consumos.Include(c => c.Veiculo);
+            return View(await applicationDbContext.ToListAsync());
         }
 
-        // GET: Veiculo/Details/5
+        // GET: Consumos/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -32,58 +33,42 @@ namespace app_web_backend.Controllers
                 return NotFound();
             }
 
-            var veiculo = await _context.Veiculos
+            var consumo = await _context.Consumos
+                .Include(c => c.Veiculo)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (veiculo == null)
+            if (consumo == null)
             {
                 return NotFound();
             }
 
-            return View(veiculo);
+            return View(consumo);
         }
-        //
-        public async Task<IActionResult> Relatorio(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
 
-            var veiculo = await _context.Veiculos
-                .Include(t => t.Consumos)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (veiculo == null)
-            {
-                return NotFound();
-            }
-
-            return View(veiculo);
-        }
-        //
-
-        // GET: Veiculo/Create
+        // GET: Consumos/Create
         public IActionResult Create()
         {
+            ViewData["VeiculoId"] = new SelectList(_context.Veiculos, "Id", "Nome");
             return View();
         }
 
-        // POST: Veiculo/Create
+        // POST: Consumos/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Placa")] Veiculo veiculo)
+        public async Task<IActionResult> Create([Bind("Id,Descricao,Data,Km,Valor,Tipo,VeiculoId")] Consumo consumo)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(veiculo);
+                _context.Add(consumo);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(veiculo);
+            ViewData["VeiculoId"] = new SelectList(_context.Veiculos, "Id", "Nome", consumo.VeiculoId);
+            return View(consumo);
         }
 
-        // GET: Veiculo/Edit/5
+        // GET: Consumos/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -91,22 +76,23 @@ namespace app_web_backend.Controllers
                 return NotFound();
             }
 
-            var veiculo = await _context.Veiculos.FindAsync(id);
-            if (veiculo == null)
+            var consumo = await _context.Consumos.FindAsync(id);
+            if (consumo == null)
             {
                 return NotFound();
             }
-            return View(veiculo);
+            ViewData["VeiculoId"] = new SelectList(_context.Veiculos, "Id", "Nome", consumo.VeiculoId);
+            return View(consumo);
         }
 
-        // POST: Veiculo/Edit/5
+        // POST: Consumos/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Placa")] Veiculo veiculo)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao,Data,Km,Valor,Tipo,VeiculoId")] Consumo consumo)
         {
-            if (id != veiculo.Id)
+            if (id != consumo.Id)
             {
                 return NotFound();
             }
@@ -115,12 +101,12 @@ namespace app_web_backend.Controllers
             {
                 try
                 {
-                    _context.Update(veiculo);
+                    _context.Update(consumo);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!VeiculoExists(veiculo.Id))
+                    if (!ConsumoExists(consumo.Id))
                     {
                         return NotFound();
                     }
@@ -131,10 +117,11 @@ namespace app_web_backend.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(veiculo);
+            ViewData["VeiculoId"] = new SelectList(_context.Veiculos, "Id", "Nome", consumo.VeiculoId);
+            return View(consumo);
         }
 
-        // GET: Veiculo/Delete/5
+        // GET: Consumos/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -142,30 +129,31 @@ namespace app_web_backend.Controllers
                 return NotFound();
             }
 
-            var veiculo = await _context.Veiculos
+            var consumo = await _context.Consumos
+                .Include(c => c.Veiculo)
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (veiculo == null)
+            if (consumo == null)
             {
                 return NotFound();
             }
 
-            return View(veiculo);
+            return View(consumo);
         }
 
-        // POST: Veiculo/Delete/5
+        // POST: Consumos/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var veiculo = await _context.Veiculos.FindAsync(id);
-            _context.Veiculos.Remove(veiculo);
+            var consumo = await _context.Consumos.FindAsync(id);
+            _context.Consumos.Remove(consumo);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool VeiculoExists(int id)
+        private bool ConsumoExists(int id)
         {
-            return _context.Veiculos.Any(e => e.Id == id);
+            return _context.Consumos.Any(e => e.Id == id);
         }
     }
 }
---
+++ app-web-backend/Models/Consumo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Obrigatório informar a descrição!")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a data!")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar a quilometragem!")]
+        public int Km { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório informar o valor!")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Valor { get; set; }
+
+        [Display(Name = "Tipo de Combustível")]
+        [Required(ErrorMessage = "Obrigatório informar o tipo de combustível!")]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Display(Name = "Veiculo")]
+        [Required(ErrorMessage = "Obrigatório informar o tipo de combustível!")]
+        public int VeiculoId { get; set; }
+
+        [ForeignKey("VeiculoId")]
+        public Veiculo Veiculo { get; set; }
+    }
+
+    public enum TipoCombustivel
+    {
+        Gasolina,
+        Etanol
+    }
+}
